067aaaf94a033873f4bebbe608ceccd82820fbb0
k0825 <kazukkiyo@icloud.com>
2023-06-17 19:45:59 +0900

    Python側の関数名を修正

--- lambda/src/handler.py
+++ lambda/src/handler.py
@@ -1,7 +1,7 @@
 import ast
 import json
 
-def handler(event, context):
+def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
